c9ad1569872876208f3c855a9162978842058e6e
mjaow1233 <nilsson.daniel1@gmail.com>
2025-09-25 10:40:06 +0200

    Skelett

---
+++ Dagsboksapp/Diary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dagsboksapp
+{
+    public class DiaryEntry
+    {
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+    internal class Diary
+
+
+    {
+    }
+}
